9ff70dd1ded69e53c597984bd9df218c9384cdc3
YanVL <yanvitor.gti@gmail.com>
2026-08-10 08:58:49 -0300

    Adicionando novas imagens e atualizando animações de habilidades no site

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
     .delete(2)
     .type("Php", { delay: 3000 })
     .delete(3)
+    .type("Laravel", { delay: 3000 })
+    .delete(7)
+    .type("Wordpress", { delay: 3000 })
+    .delete(10)
+    .type("Hubspot", { delay: 3000 })
+    .delete(7)
+    .type("Docker", { delay: 3000 })
+    .delete(6)
 
     .go();
 });
