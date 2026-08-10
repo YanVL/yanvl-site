12a3aeedb8c1d9ab1bcfedc63c251728beae1e3c
YanVL <yanvitor.gti@gmail.com>
2026-08-10 11:37:02 -0300

    Update hero title animations to use "Websites" and "Javascript" for improved accuracy

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,9 @@ document.addEventListener("DOMContentLoaded", () => {
     speed: 200,
     loop: true,
   })
-    .type("Web", { delay: 3000 })
+    .type("Websites", { delay: 3000 })
     .delete(3)
-    .type("Js", { delay: 3000 })
+    .type("Javascript", { delay: 3000 })
     .delete(2)
     .type("Php", { delay: 3000 })
     .delete(3)
